e4fb1328fd03c8871c9b6deefe8f2da583886916
AKM Miraz Hossain <akmmirazhossain@gmail.com>
2021-11-21 14:30:50 +0600

    AKM WP headless

--- src/FetchApi.js
+++ src/FetchApi.js
@@ -1,25 +1,30 @@
 import React from 'react';
-import DOMPurify from 'dompurify';
-
 import axios from 'axios';
+
 
 export default class PersonList extends React.Component {
   state = {
-    persons: []
+    persons: [],
+    loading: true
   }
 
   componentDidMount() {
     axios.get(`https://dev-akm-wp-dcop.pantheonsite.io/wp-json/wp/v2/posts`)
       .then(res => {
         const persons = res.data;
-        this.setState({ persons });
+        this.setState({ persons, loading: false });
       })
   }
 
   render() {
     return (
       <ul>
-        { this.state.persons.map(person => 
+        <header>This is a simple api fetch project built with react. <br/> 
+        Json URL: https://dev-akm-wp-dcop.pantheonsite.io/wp-json/wp/v2/posts <br/> 
+        </header>
+
+        
+        {  !this.state.loading ? this.state.persons.map(person => 
         <li>
           <h3>{person.title.rendered}</h3>
           <hr />
@@ -31,7 +36,7 @@ export default class PersonList extends React.Component {
           </div>
         </li>
         
-        )}
+        ) : <p> Loading ...</p>}
         
        
       </ul>
